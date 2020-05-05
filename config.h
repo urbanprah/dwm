47ca5b9502d25837946bab3e9a5212fc87505cd1
Urban Prah <urban.prah@protonmail.com>
2020-05-05 16:18:29 +0200

    Installed focusadjacenttag patch (which uses gcc builtins: new improvement)

--- config.h
+++ config.h
@@ -121,64 +121,64 @@ static const char *scratchpadcmd[] = { "st" , "-t", scratchpadname, "-g", "70x30
 
 #include "movestack.c"
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-        //{ MODKEY|ShiftMask,             XK_h,      setsmfact,      {.f = +0.05} },
-	//{ MODKEY|ShiftMask,             XK_l,      setsmfact,      {.f = -0.05} },
-	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-        { MODKEY|ControlMask,           XK_j,      moveresize,     {.v = (int []){ 0, 50, 0, 0 }}},
-        { MODKEY|ControlMask,           XK_k,      moveresize,     {.v = (int []){ 0, -50, 0, 0 }}},
-        { MODKEY|ControlMask,           XK_l,      moveresize,     {.v = (int []){ 50, 0, 0, 0 }}},
-        { MODKEY|ControlMask,           XK_h,      moveresize,     {.v = (int []){ -50, 0, 0, 0 }}},
-        { MODKEY|ControlMask|ShiftMask, XK_j,      moveresize,     {.v = (int []){ 0, 0, 0, 50 }}},
-        { MODKEY|ControlMask|ShiftMask, XK_k,      moveresize,     {.v = (int []){ 0, 0, 0, -50 }}},
-        { MODKEY|ControlMask|ShiftMask, XK_l,      moveresize,     {.v = (int []){ 0, 0, 50, 0 }}},
-        { MODKEY|ControlMask|ShiftMask, XK_h,      moveresize,     {.v = (int []){ 0, 0, -50, 0 }}},
-        { MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_v,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[5]} },
-	/* { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
-	/* { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, */
-	{ MODKEY|ControlMask,		XK_comma,  cyclelayout,    {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier                     key              function        argument */
+	{ MODKEY,                       XK_p,            spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return,       spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,        togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_b,            togglebar,      {0} },
+	{ MODKEY,                       XK_j,            focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,            focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,            incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,            incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,            setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,            setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,            movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,            movestack,      {.i = -1 } },
+	{ MODKEY,                       XK_Return,       zoom,           {0} },
+        { MODKEY|ControlMask,           XK_j,            moveresize,     {.v = (int []){ 0, 50, 0, 0 }}},
+        { MODKEY|ControlMask,           XK_k,            moveresize,     {.v = (int []){ 0, -50, 0, 0 }}},
+        { MODKEY|ControlMask,           XK_l,            moveresize,     {.v = (int []){ 50, 0, 0, 0 }}},
+        { MODKEY|ControlMask,           XK_h,            moveresize,     {.v = (int []){ -50, 0, 0, 0 }}},
+        { MODKEY|ControlMask|ShiftMask, XK_j,            moveresize,     {.v = (int []){ 0, 0, 0, 50 }}},
+        { MODKEY|ControlMask|ShiftMask, XK_k,            moveresize,     {.v = (int []){ 0, 0, 0, -50 }}},
+        { MODKEY|ControlMask|ShiftMask, XK_l,            moveresize,     {.v = (int []){ 0, 0, 50, 0 }}},
+        { MODKEY|ControlMask|ShiftMask, XK_h,            moveresize,     {.v = (int []){ 0, 0, -50, 0 }}},
+        { MODKEY,                       XK_minus,        setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_equal,        setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_equal,        setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_Tab,          view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,            killclient,     {0} },
+	{ MODKEY,                       XK_t,            setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_v,            setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,            setlayout,      {.v = &layouts[5]} },
+	{ MODKEY|ControlMask,		XK_comma,        cyclelayout,    {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period,       cyclelayout,    {.i = +1 } },
+	{ MODKEY,                       XK_space,        setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,        togglefloating, {0} },
+	{ MODKEY,                       XK_0,            view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,            tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,        focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,       focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,        tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,       tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_F5,           xrdb,           {.v = NULL } },
+	{ MODKEY,                       XK_minus,        setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_equal,        setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_equal,        setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_bracketleft,  viewtoleft,     {0} },
+	{ MODKEY,                       XK_bracketright, viewtoright,    {0} },
+	{ MODKEY|ShiftMask,             XK_bracketleft,  tagtoleft,      {0} },
+	{ MODKEY|ShiftMask,             XK_bracketright, tagtoright,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,            quit,           {0} },
+	TAGKEYS(                        XK_1,                            0)
+	TAGKEYS(                        XK_2,                            1)
+	TAGKEYS(                        XK_3,                            2)
+	TAGKEYS(                        XK_4,                            3)
+	TAGKEYS(                        XK_5,                            4)
+	TAGKEYS(                        XK_6,                            5)
+	TAGKEYS(                        XK_7,                            6)
+	TAGKEYS(                        XK_8,                            7)
+	TAGKEYS(                        XK_9,                            8)
 };
 
 /* button definitions */
--- dwm.c
+++ dwm.c
@@ -275,6 +275,8 @@ static void spawn(const Arg *arg);
 static Monitor *systraytomon(Monitor *m);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
+static void tagtoleft(const Arg *arg);
+static void tagtoright(const Arg *arg);
 static void tile(Monitor *);
 static void bstack(Monitor *);
 static void bstackhoriz(Monitor *);
@@ -300,6 +302,8 @@ static void updatetitle(Client *c);
 static void updatewindowtype(Client *c);
 static void updatewmhints(Client *c);
 static void view(const Arg *arg);
+static void viewtoleft(const Arg *arg);
+static void viewtoright(const Arg *arg);
 static Client *wintoclient(Window w);
 static Monitor *wintomon(Window w);
 static Client *wintosystrayicon(Window w);
@@ -2051,6 +2055,28 @@ setclientstate(Client *c, long state)
 		PropModeReplace, (unsigned char *)data, 2);
 }
 
+void
+tagtoleft(const Arg *arg) {
+	if(selmon->sel != NULL
+	&& __builtin_popcount(selmon->tagset[selmon->seltags] & TAGMASK) == 1
+	&& selmon->tagset[selmon->seltags] > 1) {
+		selmon->sel->tags >>= 1;
+		focus(NULL);
+		arrange(selmon);
+	}
+}
+
+void
+tagtoright(const Arg *arg) {
+	if(selmon->sel != NULL
+	&& __builtin_popcount(selmon->tagset[selmon->seltags] & TAGMASK) == 1
+	&& selmon->tagset[selmon->seltags] & (TAGMASK >> 1)) {
+		selmon->sel->tags <<= 1;
+		focus(NULL);
+		arrange(selmon);
+	}
+}
+
 int
 sendevent(Window w, Atom proto, int mask, long d0, long d1, long d2, long d3, long d4)
 {
@@ -2942,6 +2968,28 @@ view(const Arg *arg)
 
 	focus(NULL);
 	arrange(selmon);
+}
+
+void
+viewtoleft(const Arg *arg) {
+	if(__builtin_popcount(selmon->tagset[selmon->seltags] & TAGMASK) == 1
+	&& selmon->tagset[selmon->seltags] > 1) {
+		selmon->seltags ^= 1; /* toggle sel tagset */
+		selmon->tagset[selmon->seltags] = selmon->tagset[selmon->seltags ^ 1] >> 1;
+		focus(NULL);
+		arrange(selmon);
+	}
+}
+
+void
+viewtoright(const Arg *arg) {
+	if(__builtin_popcount(selmon->tagset[selmon->seltags] & TAGMASK) == 1
+	&& selmon->tagset[selmon->seltags] & (TAGMASK >> 1)) {
+		selmon->seltags ^= 1; /* toggle sel tagset */
+		selmon->tagset[selmon->seltags] = selmon->tagset[selmon->seltags ^ 1] << 1;
+		focus(NULL);
+		arrange(selmon);
+	}
 }
 
 Client *
